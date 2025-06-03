8d3e971ec03619a63a67971be8fef980715814a3
Rodrigo Felisberto <rodrigo.fdlira@gmail.com>
2025-06-03 12:46:08 -0300

    feat(ingestion): implement pagination and filter by emission year in API request

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,25 +1,49 @@
 import requests
 
 
-def get_nfe_data():
+def get_nfe_data(codigo_orgao, ano_emissao):
     url = "https://api.portaldatransparencia.gov.br/api-de-dados/notas-fiscais"
     key = ""
 
-    params = {"codigoOrgao": "36000", "pagina": 1}
-    headers = {"accept": "*/*", "chave-api-dados": key}
+    all_data = []
 
-    try:
-        response = requests.get(url, params=params, headers=headers)
-        response.raise_for_status()
-        data_json = response.json()
-        return data_json
-    except requests.exceptions.HTTPError as http_err:
-        print(f"Erro HTTP: {http_err}")
-    except requests.exceptions.RequestException as req_err:
-        print(f"Erro de conexão: {req_err}")
-    except Exception as e:
-        print(f"Erro inesperado: {e}")
+    while True:
+        params = {"codigoOrgao": codigo_orgao, "pagina": 1}
+        headers = {"accept": "*/*", "chave-api-dados": key}
+
+        try:
+            response = requests.get(url, params=params, headers=headers)
+            response.raise_for_status()
+            data = response.json()
+
+            if not data:
+                break
+
+            filtered_data = [
+                item
+                for item in data
+                if item.get("dataEmissao", "").endswith("/" + ano_emissao)
+            ]
+
+            all_data.extend(filtered_data)
+            print(
+                f"Página {params["pagina"]} - {len(filtered_data)} registros de 2025 encontrados"
+            )
+
+            params["pagina"] += 1
+
+        except requests.exceptions.HTTPError as http_err:
+            print(f"Erro HTTP: {http_err}")
+            break
+        except requests.exceptions.RequestException as req_err:
+            print(f"Erro de conexão: {req_err}")
+            break
+        except Exception as e:
+            print(f"Erro inesperado: {e}")
+            break
+
+    return all_data
 
 
 if __name__ == "__main__":
-    get_nfe_data()
+    get_nfe_data(codigo_orgao="3600", ano_emissao="2025")
