a401532d95eb391968e11f4d3e529f7d2450f973
Rodrigo Felisberto <rodrigo.fdlira@gmail.com>
2025-06-03 11:08:59 -0300

    feat(ingestion): improve error handling in get_nfe_data function

--- src/ingestion.py
+++ src/ingestion.py
@@ -3,16 +3,22 @@ import requests
 
 def get_nfe_data():
     url = "https://api.portaldatransparencia.gov.br/api-de-dados/notas-fiscais"
-    key_api = ""
+    key = ""
 
     params = {"codigoOrgao": "36000", "pagina": 1}
-    headers = {"accept": "*/*", "chave-api-dados": key_api}
+    headers = {"accept": "*/*", "chave-api-dados": key}
 
-    response = requests.get(url, params=params, headers=headers)
-
-    dados = response.json()
-
-    dados
+    try:
+        response = requests.get(url, params=params, headers=headers)
+        response.raise_for_status()
+        data_json = response.json()
+        return data_json
+    except requests.exceptions.HTTPError as http_err:
+        print(f"Erro HTTP: {http_err}")
+    except requests.exceptions.RequestException as req_err:
+        print(f"Erro de conexão: {req_err}")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
 
 
 if __name__ == "__main__":
