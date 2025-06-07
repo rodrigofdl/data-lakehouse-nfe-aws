80d9a1e326a0db3ee92c89d6f73126c298955b28
Rodrigo Felisberto <rodrigo.fdlira@gmail.com>
2025-06-06 22:51:35 -0300

    docs(test): add docstrings to test functions

--- tests/unit/test_ingestion_api.py
+++ tests/unit/test_ingestion_api.py
@@ -5,6 +5,9 @@ import src.ingestion_api
 
 @pytest.fixture
 def mock_nfe_data():
+    """
+    Pytest fixture to provide mock data for testing.
+    """
     return {
         "nfe_2025": [
             {"dataEmissao": "01/01/2025", "id": 1},
@@ -23,11 +26,17 @@ def mock_nfe_data():
 
 @pytest.fixture(autouse=True)
 def set_api_key(monkeypatch):
+    """
+    Pytest fixture to set the API key environment variable with a fictional value for testing.
+    """
     monkeypatch.setenv("API_KEY", "test_api_key")
 
 
 @pytest.mark.unit
 def test_request_nfe_success(mocker):
+    """
+    Tests if the `request_nfe' function returns the data correctly when the API responds successfully (status 200).
+    """
     mock_get = mocker.patch("requests.get")
     mock_get.return_value.status_code = 200
     mock_get.return_value.json.return_value = [{"data": "example"}]
@@ -38,6 +47,9 @@ def test_request_nfe_success(mocker):
 
 
 def test_request_nfe_failure(mocker):
+    """
+    Tests if the `request_nfe' function tries again and throws exception after request failures.
+    """
     mock_get = mocker.patch("requests.get")
     mock_get.side_effect = requests.exceptions.RequestException("API error")
 
@@ -49,6 +61,9 @@ def test_request_nfe_failure(mocker):
 
 
 def test_filter_nfe_per_year_sucess(mock_nfe_data):
+    """
+    Tests if the `filter_nfe_per_year` function correctly filters data by year.
+    """
     filtered_data = src.ingestion_api.filter_nfe_per_year(
         api_response=mock_nfe_data["mixed_nfe"], year_emission=2025
     )
@@ -58,6 +73,9 @@ def test_filter_nfe_per_year_sucess(mock_nfe_data):
 
 
 def test_filter_nfe_per_year_no_match(mock_nfe_data):
+    """
+    Tests if the `filter_nfe_per_year` function returns an empty list when no data matches the specified year.
+    """
     filtered_data = src.ingestion_api.filter_nfe_per_year(
         api_response=mock_nfe_data["mixed_nfe"], year_emission=2100
     )
@@ -66,6 +84,10 @@ def test_filter_nfe_per_year_no_match(mock_nfe_data):
 
 
 def test_get_nfe_data_success(mocker, mock_nfe_data):
+    """
+    Tests the complete flow of API data with multiple pages,
+    returning only data with an issuance year equal to the specified year.
+    """
     simulated_api_response = [mock_nfe_data["mixed_nfe"], mock_nfe_data["nfe_2024"], []]
     mock_request_nfe = mocker.patch(
         "src.ingestion_api.request_nfe", side_effect=simulated_api_response
@@ -79,6 +101,9 @@ def test_get_nfe_data_success(mocker, mock_nfe_data):
 
 
 def test_get_nfe_data_empty_first_page(mocker):
+    """
+    Tests if the `get_nfe_data` function returns an empty list when the first page of the API response is empty.
+    """
     mock_request_nfe = mocker.patch("src.ingestion_api.request_nfe", return_value=[])
 
     result = src.ingestion_api.get_nfe_data(organ_code="36000", year_emission=2025)
@@ -88,6 +113,9 @@ def test_get_nfe_data_empty_first_page(mocker):
 
 
 def test_get_nfe_data_reaches_max_pages(mocker, mock_nfe_data):
+    """
+    Tests if the `get_nfe_data` function stops seeking after reaching the pages limit (max_pages).
+    """
     mock_request_nfe = mocker.patch(
         "src.ingestion_api.request_nfe", return_value=mock_nfe_data["nfe_2025"]
     )
@@ -101,6 +129,9 @@ def test_get_nfe_data_reaches_max_pages(mocker, mock_nfe_data):
 
 
 def test_get_nfe_data_api_exception(mocker, mock_nfe_data):
+    """
+    Tests if the `get_nfe_data` function handles API exceptions correctly,
+    """
     simulated_api_response = [
         mock_nfe_data["nfe_2025"],
         Exception("API error"),
